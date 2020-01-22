e44b1f406c2cebbdffe5feb39621365ff00ed5a7
adriankiprono <tuimuradrian6@gmail.com>
2020-01-22 10:27:49 +0300

    add static

--- jobapp/urls.py
+++ jobapp/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url,include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^employer/',include('employer.urls'))
+    url(r'^',include('employer.urls'))
 ]
