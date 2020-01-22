2f5c69f7ec4cf5440805ee2ce1130925eb4c28dc
adriankiprono <tuimuradrian6@gmail.com>
2020-01-22 13:11:39 +0300

    add models

--- employer/models.py
+++ employer/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Applicant(models.Model):
+    
+    first_name = models.CharField(max_length =30,null=True)
+    last_name = models.CharField(max_length =30,null=True)
+    email = models.EmailField()
+    APPLICANT_CHOICES=(
+        ('EMPLOYER','employer'),
+        ('EMPLOYEE','employee'),
+    )
+    applicants=models.CharField(
+        max_length=20,
+        choices=APPLICANT_CHOICES,
+    )
+    jobs=models.CharField(max_length=50,null=True)
+    descriptions=models.TextField(max_length=200,null=True)
+    pay=models.PositiveIntegerField()
+    user=models.ForeignKey('User',on_delete=models.CASCADE,null=True)
+
+class User(models.Model):
+    user_name=models.CharField(max_length=50,null=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True,related_name='profile')
+
+
+    
+
