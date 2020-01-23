fbf2c672dd2616d795d678f178de1d5b7103d2ca
adriankiprono <tuimuradrian6@gmail.com>
2020-01-23 12:22:13 +0300

    add tinymce

--- employer/models.py
+++ employer/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -18,7 +19,7 @@ class Applicant(models.Model):
     job=models.CharField(max_length=50,null=True)
     descriptions=models.TextField(max_length=200,null=True)
     pay=models.PositiveIntegerField()
-    user=models.ForeignKey('User',on_delete=models.CASCADE,null=True)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     picture = models.ImageField(upload_to='avatar/', default='default.jpg')
 
     @classmethod
